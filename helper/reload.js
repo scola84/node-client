455dd74be78a68ca849803c9df94f57f412cbae4
M.C. van den Boogaart <scola@kvdb.name>
2016-03-17 19:37:34 +0100

    Remove reload script: added to server

--- helper/reload.js
+++
@@ -1,4 +0,0 @@
-const element = document.createElement('script');
-element.type = 'text/javascript';
-element.src = '//' + location.hostname + ':35729/livereload.js?snipver=1';
-document.head.appendChild(element);
